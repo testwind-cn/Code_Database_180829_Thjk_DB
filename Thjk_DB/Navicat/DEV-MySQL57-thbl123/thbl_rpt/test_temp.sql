facba0eb5d788fd9fdbdf1f5093d010e9f698728
acewind1 <36976754+acewind1@users.noreply.github.com>
2019-07-14 00:04:42 +0800

    190713_移走Hive_100、Mysql、Navicat，放入Files项目中

--- Thjk_DB/Navicat/DEV-MySQL57-thbl123/thbl_rpt/test_temp.sql
+++
@@ -1,5 +0,0 @@
-SELECT
-	date_v, f_date_normal_e(date_v,'2015-08-03',14),  f_get_ma_len(date_v,'2015-08-03',14,5),
-	f_get_ma_len(`date_v`,'2015-08-03',1,14.0)
-FROM
-dim_date
--- Thjk_DB/Navicat/prod-db-from-bl-mysql/goodboss/test.sql
+++
@@ -1,4 +0,0 @@
-SELECT *
-  from gb_loan_application_kkd where mcht_type in ( 'S','V')
-
-
